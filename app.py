6af38817a55f3bd6d30f0e831fa1b306e7890dca
Zijian Zhou <franciszzj@gmail.com>
2024-12-10 22:32:44 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image
+from huggingface_hub import snapshot_download
 from leffa.transform import LeffaTransform
 from leffa.model import LeffaModel
 from leffa.inference import LeffaInference
@@ -7,6 +8,9 @@ from utils.garment_agnostic_mask_predictor import AutoMasker
 from utils.densepose_predictor import DensePosePredictor
 
 import gradio as gr
+
+# Download checkpoints
+snapshot_download(repo_id="franciszzj/Leffa", local_dir="./")
 
 
 def leffa_predict(src_image_path, ref_image_path, control_type):
@@ -78,43 +82,56 @@ if __name__ == "__main__":
     # control_type = sys.argv[3]
     # leffa_predict(src_image_path, ref_image_path, control_type)
 
-    # Launch a gr.Interface
-    gr_demo = gr.Interface(
-        fn=leffa_predict,
-        inputs=[
-            gr.Image(sources=["upload", "webcam", "clipboard"],
-                     type="filepath",
-                     label="Source Person Image",
-                     width=768,
-                     height=1024,
-                     ),
-            gr.Image(sources=["upload", "webcam", "clipboard"],
-                     type="filepath",
-                     label="Reference Image",
-                     width=768,
-                     height=1024,
-                     ),
-            gr.Radio(["virtual_tryon", "pose_transfer"],
-                     label="Control Type",
-                     default="virtual_tryon",
-                     ),
-        ],
-        outputs=[
-            gr.Image(label="Generated Person Image",
-                     width=768,
-                     height=1024,
-                     )
-        ],
-        title="Leffa",
-        description="Leffa is a unified framework for controllable person image generation that enables precise manipulation of both appearance (i.e., virtual try-on) and pose (i.e., pose transfer).",
-        article="Controllable person image generation aims to generate a person image conditioned on reference images, allowing precise control over the person’s appearance or pose. However, prior methods often distort fine-grained textural details from the reference image, despite achieving high overall image quality. We attribute these distortions to inadequate attention to corresponding regions in the reference image. To address this, we thereby propose \textbf{learning flow fields in attention} (\textbf{\ours{}}), which explicitly guides the target query to attend to the correct reference key in the attention layer during training. Specifically, it is realized via a regularization loss on top of the attention map within a diffusion-based baseline. Our extensive experiments show that Leffa achieves state-of-the-art performance in controlling appearance (virtual try-on) and pose (pose transfer), significantly reducing fine-grained detail distortion while maintaining high image quality. Additionally, we show that our loss is model-agnostic and can be used to improve the performance of other diffusion models.",
-        examples=[
-            ["./examples/14092_00_person.jpg", "./examples/04181_00_garment.jpg", "virtual_tryon"],
-            ["./examples/14092_00_person.jpg", "./examples/14684_00_person.jpg", "pose_transfer"],
-        ],
-        # cache_examples=True,
-        examples_per_page=10,
-        allow_flagging=False,
-        theme=gr.themes.Default(),
-    )
-    gr_demo.launch(share=True, server_port=7860)
+    with gr.Blocks().queue() as demo:
+        gr.Markdown(
+            "## Leffa: Learning Flow Fields in Attention for Controllable Person Image Generation")
+        gr.Markdown("Leffa is a unified framework for controllable person image generation that enables precise manipulation of both appearance (i.e., virtual try-on) and pose (i.e., pose transfer).")
+        with gr.Row():
+            with gr.Column():
+                src_image = gr.Image(
+                    sources=["upload"],
+                    type="filepath",
+                    label="Source Person Image",
+                    width=384,
+                    height=512,
+                )
+                with gr.Row():
+                    control_type = gr.Dropdown(
+                        ["virtual_tryon", "pose_transfer"], label="Control Type")
+
+                example = gr.Examples(
+                    inputs=src_image,
+                    examples_per_page=10,
+                    examples=["./examples/14684_00_person.jpg",
+                              "./examples/14092_00_person.jpg"],
+                )
+
+            with gr.Column():
+                ref_image = gr.Image(
+                    sources=["upload"],
+                    type="filepath",
+                    label="Reference Image",
+                    width=384,
+                    height=512,
+                )
+                with gr.Row():
+                    gen_button = gr.Button("Generate")
+
+                example = gr.Examples(
+                    inputs=ref_image,
+                    examples_per_page=10,
+                    examples=["./examples/04181_00_garment.jpg",
+                              "./examples/14684_00_person.jpg"],
+                )
+
+            with gr.Column():
+                gen_image = gr.Image(
+                    label="Generated Person Image",
+                    width=384,
+                    height=512,
+                )
+
+            gen_button.click(fn=leffa_predict, inputs=[
+                             src_image, ref_image, control_type], outputs=[gen_image])
+
+        demo.launch(share=True, server_port=7860)
